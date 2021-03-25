386bc9d30b00302d5e2535d4b597730ec541aa5e
Nataliia Iliushchenko <iliushchenko.nataliia@gmail.com>
2021-03-25 14:00:55 +0200

    Deprecated syntax was fixed.

--- src/CRM/Exception/Logger.php
+++ src/CRM/Exception/Logger.php
@@ -1,50 +1,55 @@
 <?php
+
 namespace Zoho\CRM\Exception;
 
-use Zoho\CRM\Common\ZCRMConfigUtil;
 use Zoho\Resources\Configurations;
 
 class Logger
 {
-    public static function writeToFile($msg) {
+    public static function writeToFile($msg)
+    {
         $config = Configurations::getInstance();
         $logFilePath = $config->getAppLogFilePath();
-        
+
         if (empty($logFilePath)) {
             throw new \Exception('Log file path is not set.');
         }
-        
+
         set_include_path($logFilePath);
         $path = get_include_path();
-        if ($path{strlen($path)-1} != '\/') {
+        if ($path[strlen($path) - 1] != '\/') {
             $path = $path . '/';
         }
-        
+
         $path = str_replace("\n", "", $path);
-        $filePointer = fopen($path."ZCRMClientLibrary.log", "a");
+        $filePointer = fopen($path . "ZCRMClientLibrary.log", "a");
         if (!$filePointer) {
             return;
         }
         fwrite($filePointer, sprintf("%s %s\n", date("Y-m-d H:i:s"), $msg));
         fclose($filePointer);
     }
-    
+
     public static function warn($msg)
     {
         self::writeToFile("WARNING: $msg");
     }
+
     public static function info($msg)
     {
         self::writeToFile("INFO: $msg");
     }
+
     public static function severe($msg)
     {
         self::writeToFile("SEVERE: $msg");
     }
+
     public static function err($msg)
     {
         self::writeToFile("ERROR: $msg");
     }
+
     public static function debug($msg)
     {
         self::writeToFile("DEBUG: $msg");
